ac0adbddbdc5683a2c9172b754b3a7a10ffbfa07
ZolsKe <zoltan.the.kepes@gmail.com>
2022-08-30 20:44:17 +0100

    add 3 empty links

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Sidebar = () => (
+  <div>
+    <a href="#">My Photos</a>
+    <a href="#">My Illustrations</a>
+    <a href="#">My Paintings</a>
+  </div>
+);
